96dceeb2609166153fa0608aa2b90d99b495ba22
Dorin Flocos <dflocos@gmail.com>
2024-11-03 20:14:55 -0500

    Initial Commit

---
+++ bean/beanApp.swift
@@ -0,0 +1,17 @@
+//
+//  beanApp.swift
+//  bean
+//
+//  Created by dorin flocos on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct beanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
